002b3e84dca5f398e1146abfa739ae17aa0afaf8
Wdchopehd <cedric.xu.0408@gmail.com>
2023-10-06 11:56:36 +0800

    renew the server file

--- server.c
+++ server.c
@@ -1,50 +1,112 @@
-/* A simple server in the internet domain using TCP
-   The port number is passed as an argument */
 #include <stdio.h>
-#include <sys/types.h> 
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <stdlib.h> // Include this for the exit function
-#include <string.h>
+#include <arpa/inet.h> // 包含inet_ntoa函数的头文件
+#include <pthread.h>
 #include <unistd.h>
+#include <fcntl.h>
 
-void error(char *msg)
-{
-    perror(msg);
-    exit(1);
+#define MYPORT 3456
+#define BACKLOG 10
+#define MAX_CLIENTS 100
+
+struct Client {
+    int socket;
+    char buffer[256];
+};
+
+struct Client clients[MAX_CLIENTS];
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+int client_count = 0;
+
+void* handle_client(void* arg) {
+    int client_index = *(int*)arg;
+    int client_socket = clients[client_index].socket;
+    char* buffer = clients[client_index].buffer;
+
+    fcntl(client_socket, F_SETFL, O_NONBLOCK);
+
+    while (1) {
+        int n = recv(client_socket, buffer, sizeof(buffer), 0);
+        if (n < 0) {
+            // Handle error
+            perror("recv");
+        } else if (n == 0) {
+            // Client disconnected
+            printf("Client %d disconnected\n", client_index);
+            break;
+        } else {
+            buffer[n] = '\0';
+            printf("Received from client %d: %s\n", client_index, buffer);
+            // Handle received data
+
+            // Example: send(client_socket, response, strlen(response), 0);
+        }
+    }
+
+    close(client_socket);
+    pthread_mutex_lock(&mutex);
+    client_count--;
+    pthread_mutex_unlock(&mutex);
+    pthread_exit(NULL);
 }
 
-int main(int argc, char *argv[])
-{
-     int sockfd, newsockfd, portno, clilen;
-     char buffer[256];
-     struct sockaddr_in serv_addr, cli_addr;
-     int n;
-     if (argc < 2) {
-         fprintf(stderr,"ERROR, no port provided\n");
-         exit(1);
-     }
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0) 
-        error("ERROR opening socket");
-     bzero((char *) &serv_addr, sizeof(serv_addr));
-     portno = atoi(argv[1]);
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
-     if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0) 
-              error("ERROR on binding");
-     listen(sockfd,5);
-     clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-     if (newsockfd < 0) 
-          error("ERROR on accept");
-     bzero(buffer,256);
-     n = read(newsockfd,buffer,255);
-     if (n < 0) error("ERROR reading from socket");
-     printf("Here is the message: %s\n",buffer);
-     n = write(newsockfd,"I got your message",18);
-     if (n < 0) error("ERROR writing to socket");
-     return 0; 
+int main() {
+    int sockfd, new_fd; // 声明sockfd
+    struct sockaddr_in my_addr;
+    struct sockaddr_in client_addr;
+    socklen_t sin_size;
+
+    // 创建套接字，绑定端口，监听连接
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        perror("socket");
+        exit(1);
+    }
+
+    my_addr.sin_family = AF_INET;
+    my_addr.sin_port = htons(MYPORT);
+    my_addr.sin_addr.s_addr = INADDR_ANY;
+    bzero(&(my_addr.sin_zero), 8);
+
+    if (bind(sockfd, (struct sockaddr*)&my_addr, sizeof(struct sockaddr)) == -1) {
+        perror("bind");
+        exit(1);
+    }
+
+    if (listen(sockfd, BACKLOG) == -1) {
+        perror("listen");
+        exit(1);
+    }
+
+    printf("Server started. Listening on port %d...\n", MYPORT);
+
+    while (1) {
+        sin_size = sizeof(struct sockaddr_in);
+        new_fd = accept(sockfd, (struct sockaddr*)&client_addr, &sin_size);
+        if (new_fd < 0) {
+            perror("accept");
+            continue;
+        }
+
+        printf("Server: got connection from %s\n", inet_ntoa(client_addr.sin_addr));
+
+        pthread_mutex_lock(&mutex);
+        if (client_count >= MAX_CLIENTS) {
+            printf("Too many clients. Connection rejected.\n");
+            close(new_fd);
+        } else {
+            clients[client_count].socket = new_fd;
+            pthread_t thread;
+            pthread_create(&thread, NULL, handle_client, (void*)&client_count);
+            client_count++;
+        }
+        pthread_mutex_unlock(&mutex);
+    }
+
+    close(sockfd);
+    return 0;
 }
